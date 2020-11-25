9db08f6b05316d49cd596c2a84b81bab981921c4
Karolis Kazlauskis <karolis@flumens.io>
2020-11-25 20:59:16 +0000

    Add testing rules

--- index.js
+++ index.js
@@ -7,26 +7,33 @@ module.exports = {
     "plugin:jest/recommended",
   ],
   rules: {
+    "no-underscore-dangle": 0,
     "@getify/proper-arrows/name": ["error"],
-    "react/forbid-prop-types": 0,
     "no-plusplus": 0,
-    "react/require-default-props": 0,
-    "react/sort-comp": 0,
-    "react/destructuring-assignment": 0,
-    "react/button-has-type": 0,
     "no-unused-expressions": [
       2,
       {
         allowShortCircuit: true,
       },
     ],
+
+    // react
+    "react/forbid-prop-types": 0,
+    "react/require-default-props": 0,
+    "react/sort-comp": 0,
+    "react/destructuring-assignment": 0,
+    "react/button-has-type": 0,
     "jsx-a11y/alt-text": 0,
-    "no-underscore-dangle": 0,
     "react/jsx-props-no-spreading": 0,
     "react/state-in-constructor": 0,
     "react/static-property-placement": 0,
     "react/jsx-one-expression-per-line": 0,
     "react/no-unescaped-entities": 0,
+
+    // testing
+    "jest/no-if": 2,
+    "jest/expect-expect": 2,
+    "jest/require-top-level-describe": 2,
   },
   overrides: [
     {
@@ -41,7 +48,6 @@ module.exports = {
     __DEV__: true,
     __TEST__: true,
     __PROD__: true,
-    t: true,
   },
   ignorePatterns: ["node_modules/", "dist/"],
 };
